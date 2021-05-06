35674d2933f768d4a301e107947da67033388b3a
SimonGaufreteau <simon.gaufreteau@gmail.com>
2021-05-06 11:16:06 +0200

    Adds non working version of get matches for all players

--- index.js
+++ index.js
@@ -34,8 +34,66 @@ function getMatchesIDByName(name, start = 0, count = 20) {
         }).catch((error) => console.warn(error));
 }
 
+// https://europe.api.riotgames.com/lol/match/v5/matches/EUW1_5250023009
+function getMatchByID(id) {
+    const url = `${BASE_URL}lol/match/v5/matches/${id}`;
+    return fetch(url, { headers: BASE_HEADERS }).then((res) => checkResponse(res))
+        .then((response) => response.json());
+}
+
+async function checkGamesInCommon(mainName, arrayName) {
+    const idsPlayer = [];
+    const ids = [];
+    for (player_name of arrayName) {
+        idsPlayer.push(getMatchesIDByName(player_name));
+        ids.push(getSummonerIDByName(player_name));
+    }
+    await Promise.all(idsPlayer);
+    await Promise.all(ids);
+    console.log(`ids: ${idsPlayer}`);
+    for (let i = 0; i < arrayName.length; i += 1) {
+        const playerName = arrayName[i];
+        console.log(`Player : ${playerName}`);
+        console.log(idsPlayer[i]);
+        console.log(idsPlayer[i].length);
+    }
+
+    // For each game of each player :
+    // for each other player in this game, if player in list given, add the game to the result
+    const allMatches = [];
+    for (let i = 0; i < idsPlayer.length; i += 1) {
+        const matchesId = idsPlayer[i];
+        console.log(`Matchesid : ${i}`);
+        console.log(matchesId);
+        console.log(`len : ${matchesId.length}`);
+        const tempArray = [];
+        // eslint-disable-next-line no-restricted-syntax
+        for (let j = 0; j < matchesId.length; j += 1) {
+            const id = matchesId[j];
+            console.log(j);
+            tempArray.push(getMatchByID(id));
+        }
+        console.log(`allmatches[${i}]`);
+        console.log(tempArray[i]);
+        await Promise.all(tempArray);
+        allMatches.push(tempArray);
+    }
+    await Promise.all(allMatches);
+    console.log(allMatches);
+}
+
 function main() {
-    getMatchesIDByName('3x1').then((matches) => console.log(`Matches : ${matches}`));
+    getMatchesIDByName('3x1').then((matches) => {
+        console.log(`Matches : ${matches}`);
+        getMatchByID(matches[0]).then(
+            (match) => {
+                // console.log('Match : ');
+                // console.log(match);
+                // console.log('Participant 0 : ');
+                // console.log(match.info.participants[0]);
+            },
+        );
+    }).then(checkGamesInCommon('3x1', ['Lwipon', 'Ailler', 'Erebz']));
 }
 
 main();
