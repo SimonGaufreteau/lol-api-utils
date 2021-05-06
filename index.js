3ad4373f260f1597c76ce0d5cc960e400881ec16
Simon Gaufreteau <simon.gaufreteau@gmail.com>
2021-05-06 14:17:20 +0200

    Adds gathering and print of matches

--- index.js
+++ index.js
@@ -1,99 +1,192 @@
-require('dotenv').config();
-const fetch = require('node-fetch');
+/* eslint-disable no-restricted-syntax */
+import { RateLimiter } from 'limiter';
+import { config } from 'dotenv';
+import fetch from 'node-fetch';
+import fs from 'fs';
+
+config();
+
+const limiter = new RateLimiter({ tokensPerInterval: 3, interval: 'second' });
 
 const BASE_URL = 'https://europe.api.riotgames.com/';
 const BASE_HEADERS = {
     'X-Riot-Token': process.env.API_KEY,
 };
 
+const FAILED = [];
+
 function checkResponse(res) {
     if (res.ok) {
         return res;
     }
-    return Promise.reject(new Error('Network response was not ok'));
+    const msg = `${res.status}:${res.statusText} (${res.url})`;
+    console.log(msg);
+    return Promise.reject(new Error(msg));
 }
 
-function getSummonerIDByName(name) {
+async function getSummonerIDByName(name) {
     const url = `${BASE_URL}riot/account/v1/accounts/by-riot-id/${name}/EUW`;
+    await limiter.removeTokens(1);
     return fetch(url, { headers: BASE_HEADERS })
         .then((res) => checkResponse(res))
         .then((response) => response.json())
         .then((data) => data.puuid)
-        .catch((error) => console.warn(error));
+        .catch((error) => FAILED.push(error.message));
 }
 
-function getMatchesIDByName(name, start = 0, count = 20) {
-    return getSummonerIDByName(name)
-        .then((id) => {
-            console.log(`id : ${id}`);
-            const url = `${BASE_URL}lol/match/v5/matches/by-puuid/${id}/ids?start=${start}&count=${count}`;
-            const result = fetch(url, { headers: BASE_HEADERS })
-                .then((res) => checkResponse(res))
-                .then((response) => response.json());
-            return result;
-        }).catch((error) => console.warn(error));
+async function getMatchesID(summonerId, start = 0, count = 20) {
+    console.log(`id : ${summonerId}`);
+    const url = `${BASE_URL}lol/match/v5/matches/by-puuid/${summonerId}/ids?start=${start}&count=${count}`;
+    await limiter.removeTokens(1);
+    const result = fetch(url, { headers: BASE_HEADERS })
+        .then((res) => checkResponse(res))
+        .then((response) => response.json());
+    return result;
 }
 
 // https://europe.api.riotgames.com/lol/match/v5/matches/EUW1_5250023009
-function getMatchByID(id) {
+async function getMatchByID(id) {
     const url = `${BASE_URL}lol/match/v5/matches/${id}`;
+    await limiter.removeTokens(1);
     return fetch(url, { headers: BASE_HEADERS }).then((res) => checkResponse(res))
-        .then((response) => response.json());
+        .then((response) => response.json())
+        .catch((error) => {
+            FAILED.push(error.message);
+            return undefined;
+        });
 }
 
 async function checkGamesInCommon(mainName, arrayName) {
-    const idsPlayer = [];
-    const ids = [];
-    for (player_name of arrayName) {
-        idsPlayer.push(getMatchesIDByName(player_name));
-        ids.push(getSummonerIDByName(player_name));
+    let idsPlayer = [];
+    let ids = [];
+    for (const playerName of arrayName) {
+        ids.push(getSummonerIDByName(playerName));
     }
-    await Promise.all(idsPlayer);
-    await Promise.all(ids);
+    ids = await Promise.all(ids);
+    for (const id of ids) {
+        idsPlayer.push(getMatchesID(id));
+    }
+    idsPlayer = await Promise.all(idsPlayer);
     console.log(`ids: ${idsPlayer}`);
     for (let i = 0; i < arrayName.length; i += 1) {
         const playerName = arrayName[i];
+        const matchesId = idsPlayer[i];
         console.log(`Player : ${playerName}`);
-        console.log(idsPlayer[i]);
-        console.log(idsPlayer[i].length);
+        console.log(matchesId);
+        console.log(matchesId.length);
     }
 
     // For each game of each player :
     // for each other player in this game, if player in list given, add the game to the result
-    const allMatches = [];
+    let allMatches = [];
     for (let i = 0; i < idsPlayer.length; i += 1) {
         const matchesId = idsPlayer[i];
-        console.log(`Matchesid : ${i}`);
-        console.log(matchesId);
-        console.log(`len : ${matchesId.length}`);
         const tempArray = [];
         // eslint-disable-next-line no-restricted-syntax
         for (let j = 0; j < matchesId.length; j += 1) {
             const id = matchesId[j];
-            console.log(j);
             tempArray.push(getMatchByID(id));
         }
-        console.log(`allmatches[${i}]`);
-        console.log(tempArray[i]);
-        await Promise.all(tempArray);
         allMatches.push(tempArray);
     }
-    await Promise.all(allMatches);
-    console.log(allMatches);
+    allMatches = await Promise.all(allMatches.map((innerPromiseArray) => Promise.all(innerPromiseArray)));
+    return allMatches;
 }
 
-function main() {
-    getMatchesIDByName('3x1').then((matches) => {
-        console.log(`Matches : ${matches}`);
-        getMatchByID(matches[0]).then(
-            (match) => {
-                // console.log('Match : ');
-                // console.log(match);
-                // console.log('Participant 0 : ');
-                // console.log(match.info.participants[0]);
-            },
-        );
-    }).then(checkGamesInCommon('3x1', ['Lwipon', 'Ailler', 'Erebz']));
+function saveMatchesTofile(file) {
+    fs.open(file, 'w', (err) => {
+        if (err) throw err;
+    });
+    fs.writeFile(file, JSON.stringify(matches), (err) => {
+        if (err) throw err;
+    });
+}
+
+function readMatchesFromFile(file) {
+    try {
+        const data = fs.readFileSync(file, 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        console.error(err);
+    }
+    return {};
+}
+
+function removeEmptyOrNull(obj) {
+    Object.keys(obj).forEach((k) => (obj[k] && typeof obj[k] === 'object') && removeEmptyOrNull(obj[k])
+      || (!obj[k] && obj[k] !== undefined) && delete obj[k]);
+    return obj;
+}
+
+function getQueueType(id) {
+    switch (id) {
+    case 400:
+        return 'Normal';
+    case 420:
+        return 'Soloq';
+    case 440:
+        return 'Flex';
+    default
+        : return '';
+    }
+}
+
+function printMatches(matches) {
+    let maxGamePerPlayer = 0;
+    let minGamePerPlayer = 999;
+
+    let meanPlayer = 0;
+    let i = 0;
+    const tab = '    ';
+    for (const playerMatches of matches) {
+        console.log(`Logging matches for player ${i} :`);
+        let j = 0;
+        for (const game of playerMatches) {
+            if (game !== undefined) {
+                console.log(`Match ${j} :`);
+                console.log(`Mode : ${game.info.gameMode} ${getQueueType(game.info.queueId)}`);
+                console.log('Participants :');
+                for (const par of game.info.participants) {
+                    console.log(`${tab}Player ${par.summonerName} :`);
+                    console.log(`${tab}- champ : ${par.championName}`);
+                    console.log(`${tab}- position : ${par.individualPosition} or ${par.lane} `);
+
+                    console.log(`${tab}- KDA : ${par.kills}/${par.deaths}/${par.assists}`);
+                }
+                j += 1;
+            }
+        }
+        meanPlayer += j;
+        maxGamePerPlayer = Math.max(maxGamePerPlayer, j);
+        minGamePerPlayer = Math.min(minGamePerPlayer, j);
+        i += 1;
+    }
+    console.log(`Number of players : ${i + 1}`);
+    console.log(`Number of games per player (max / mean / min) : 
+    ${maxGamePerPlayer} / ${Math.round(meanPlayer / i)} / ${minGamePerPlayer}`);
+}
+
+async function main() {
+    // const matches = await getMatchesIDByName('Erebz');
+    // console.log(matches);
+    const readf = true;
+    if (readf) {
+        const file = 'saves/matches_save.json';
+        let matches = readMatchesFromFile(file);
+        console.log('----------------------------');
+        console.log('Matches');
+        console.log('----------------------------');
+        matches = removeEmptyOrNull(matches);
+        printMatches(matches);
+    } else {
+        const id = await getSummonerIDByName('3x1');
+        const matches = await getMatchesID(id, 0, 50);
+        console.log(matches);
+        for (const match of matches) {
+            const t = await getMatchByID(match);
+            console.log(t);
+        }
+    }
 }
 
 main();
